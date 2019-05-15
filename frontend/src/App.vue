43d6540213a8a06e0b45bd644874c345b0aea95d
Alexander Neznaev <corpsetrup@gmail.com>
2019-05-15 15:41:36 +0300

    nginx conf

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Clinic Application"/>
+        <MainWindow/>
     </div>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld.vue'
+    import MainWindow from './components/MainWindow.vue'
 
     export default {
         name: 'app',
         components: {
-            HelloWorld
+            MainWindow
         }
     }
 </script>
---
+++ frontend/src/components/MainWindow.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="hello">
+        <h1>Welcome to Clinic Application</h1>
+        <!--<select>
+            <option v-for="speciality in dictionary.specialities" v-bind:value="speciality.value">
+                {{ speciality.label }}
+            </option>
+        </select>-->
+        <div>
+            {{ dictionary }}
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                dictionary: []
+            };
+        },
+        mounted() {
+            axios
+                .get('http://localhost:8090/clinic/api/dictionary')
+                .then(response => (this.dictionary = response.data));
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
