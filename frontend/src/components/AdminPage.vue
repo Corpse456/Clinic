5586c08396ecbb604b02a5c08bf461f9f132577e
Alexander Neznaev <Corpsetrup@gmail.com>
2019-06-03 21:21:35 +0300

    added tables

--- frontend/src/components/AdminPage.vue
+++ frontend/src/components/AdminPage.vue
@@ -1,26 +1,99 @@
 <template>
     <div class="admin">
-        <button v-on:click="createDoctor">Hire doctor</button>&thinsp;
-        <button v-on:click="createPatient">Create patient card</button>&thinsp;
+        <button v-on:click="createDoctor">Hire doctor</button>
+        <button v-on:click="createPatient">Create patient card</button>
         <button v-on:click="createAdmin">Create new administrator</button>
-        <br/><br><input v-if="isCreateAdmin" v-model="alias" type="text" placeholder="alias"/><br/><br/>
-        <br/><br><input v-if="isCreateAdmin" v-model="password" type="password" placeholder="password"/><br/><br/>
-        <input v-if="isCreateDoctor || isCreatePatient || isCreateAdmin" v-model="name" type="text" placeholder="name"/><br/><br/>
+        <br/>
+        <button v-on:click="fillDoctorsTable()">Show all doctors</button>
+        <button v-on:click="fillPatientsTable()">Show all aptient</button>
+        <button v-on:click="fillTicketsTable()">Show all tickets</button>
+
+        <input v-if="isCreateAdmin" v-model="alias" type="text" placeholder="alias"/>
+        <input v-if="isCreateAdmin" v-model="password" type="password" placeholder="password"/>
+        <input v-if="isCreateDoctor || isCreatePatient || isCreateAdmin" v-model="name" type="text" placeholder="name"/>
         <input v-if="isCreateDoctor || isCreatePatient || isCreateAdmin" v-model="lastName" type="text"
-               placeholder="last name"/><br/><br/>
+               placeholder="last name"/>
         <input v-if="isCreateDoctor || isCreatePatient" v-model="birthDate" type="date"
-               placeholder="birth date"/><br/><br/>
+               placeholder="birth date"/>
         <select v-if="isCreateDoctor || isCreatePatient" v-model="selectedGender">
             <option v-for="gender in genders" v-bind:value="gender" v-bind:key="gender.value">
                 {{ gender[1] }}
             </option>
-        </select><br/><br/>
+        </select>
         <select v-if="isCreateDoctor" v-model="selectedSpeciality">
             <option v-for="speciality in specialities" v-bind:value="speciality" v-bind:key="speciality.id">
                 {{ speciality.name }}
             </option>
-        </select><br/><br/>
+        </select>
         <button v-if="isCreateDoctor || isCreatePatient || isCreateAdmin" v-on:click="submit">Submit</button>
+
+        <table v-if="doctors.length > 0">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Birth Date</th>
+                <th scope="col">Speciality</th>
+                <th scope="col">Special Identifier</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(doctor, index) in doctors" :key="index">
+                <td>{{doctor.id}}</td>
+                <td>{{doctor.name}}</td>
+                <td>{{doctor.lastName}}</td>
+                <td>{{genders.get(doctor.gender)}}</td>
+                <td>{{doctor.birthDate}}</td>
+                <td>{{getSpecialityName(doctor.specialityId)}}</td>
+                <!--TODO find out how show it-->
+                <td>{{doctor.specialIdentifier}}</td>
+            </tr>
+            </tbody>
+        </table>
+        <table v-if="patients.length > 0">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Birth Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(patient, index) in patients" :key="index">
+                <td>{{patient.id}}</td>
+                <td>{{patient.name}}</td>
+                <td>{{patient.lastName}}</td>
+                <td>{{genders.get(patient.gender)}}</td>
+                <td>{{patient.birthDate}}</td>
+            </tr>
+            </tbody>
+        </table>
+        <table v-if="tickets.length > 0">
+            <thead>
+            <tr>
+                <th scope="col">Patient ID</th>
+                <th scope="col">Patient First Name</th>
+                <th scope="col">Patient Last Name</th>
+                <th scope="col">Date/Time</th>
+                <th scope="col">Number</th>
+                <th scope="col">Doctor ID</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(ticket, index) in tickets" :key="index">
+                <td>{{ticket.patientId}}</td>
+                <td>{{ticket.patientName}}</td>
+                <td>{{ticket.patientLastName}}</td>
+                <td>{{ticket.dateTime}}</td>
+                <td>{{ticket.number}}</td>
+                <td>{{ticket.doctorId}}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -37,12 +110,14 @@
                 isCreateAdmin: false,
                 selectedGender: "",
                 selectedSpeciality: "",
+                doctors: [],
+                patients: [],
+                tickets: [],
                 name: "",
                 lastName: "",
                 birthDate: "",
                 alias: "",
                 password: "",
-                tickets: []
             }
         },
         computed: {
@@ -52,6 +127,30 @@
             })
         },
         methods: {
+            getSpecialityName(specialityId) {
+                return new Map(this.specialities.map(el => [el.id, el.name])).get(specialityId);
+            },
+            fillDoctorsTable() {
+                axios.post('/backend/doctor/search', {}, {
+                    headers: {
+                        Authorization: VueCookies.get('authorization')
+                    }
+                }).then(response => this.doctors = response.data.elements);
+            },
+            fillPatientsTable() {
+                axios.post('/backend/patient/search', {}, {
+                    headers: {
+                        Authorization: VueCookies.get('authorization')
+                    }
+                }).then(response => this.patients = response.data.elements);
+            },
+            fillTicketsTable() {
+                axios.post('/backend/ticket/search', {}, {
+                    headers: {
+                        Authorization: VueCookies.get('authorization')
+                    }
+                }).then(response => this.tickets = response.data.elements);
+            },
             createDoctor() {
                 this.isCreateDoctor = true;
                 this.isCreatePatient = false;
@@ -137,7 +236,46 @@
         color: #42b983;
     }
 
+    input {
+        display: block;
+        margin: 4px auto;
+    }
+
+    select {
+        display: block;
+        margin: 4px auto;
+    }
+
+    button {
+        margin: 4px;
+    }
+
     body {
         padding: 3rem;
+    }
+
+    table {
+        border: 2px solid #42b983;
+        border-radius: 3px;
+        background-color: #fff;
+    }
+
+    th {
+        background-color: #42b983;
+        color: rgba(255, 255, 255, 0.66);
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    td {
+        background-color: #f9f9f9;
+    }
+
+    th, td {
+        min-width: 120px;
+        padding: 10px 20px;
     }
 </style>
